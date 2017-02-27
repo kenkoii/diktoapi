1844eb3f75c5a2f24d200351bd89b5f6e4a29de3
Kentoy <jankenith.johannessen@gmail.com>
2017-02-27 17:51:54 +0800

    changed API

--- api/models/word.go
+++ api/models/word.go
@@ -3,23 +3,26 @@ package models
 import (
 	"encoding/json"
 	"io"
+	"log"
 	"time"
 
-	"golang.org/x/net/context"
+	"net/http"
 
 	"github.com/asaskevich/govalidator"
+	"golang.org/x/net/context"
 	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/urlfetch"
 )
 
 // Word is the model for a comment struct/object
 type Word struct {
-	Word    string    `json:"word"`
-	Lemma   string    `json:"lemma"`
+	Text    string    `json:"text"`
+	Lemma   string    `json:"lemma_text"`
 	Created time.Time `json:"created"`
 }
 
 func (word *Word) key(c context.Context) *datastore.Key {
-	return datastore.NewKey(c, "Word", word.Word, 0, nil)
+	return datastore.NewKey(c, "Word", word.Text, 0, nil)
 }
 
 func (word *Word) save(c context.Context) error {
@@ -55,15 +58,41 @@ func NewWord(c context.Context, r io.ReadCloser) (*Word, error) {
 }
 
 // GetWord takes a word and checks it in the database
-func GetWord(c context.Context, id string) (*Word, error) {
+func GetWord(c context.Context, id string) (interface{}, error) {
+	var apiURL = "https://www.enclout.com/api/v1/term/show.json?auth_token=kdXnSYS9jhyJULBXC4Bx&text="
 	var word Word
-	word.Word = id
+	word.Text = id
 
 	k := word.key(c)
 	err := datastore.Get(c, k, &word)
 	if err != nil {
-		return nil, err
-	}
+		client := urlfetch.Client(c)
+		req, err := http.NewRequest("GET", apiURL+word.Text, nil)
+		if err != nil {
+			return nil, err
+		}
+		//req.Header.Set("Content-Type", "application/json")
+		// req.Header.Set("X-Mashape-Key", "d2frRt9uQ8mshK76dIeYMr7TC8lip1XaEwcjsnjFx4ybfoajGl")
 
+		resp, err := client.Do(req)
+		// resp, err := client.Post(apiURL+word.Word, "application/json", nil)
+		if err != nil {
+			return "Search Failed: Word not found", nil
+		}
+
+		defer resp.Body.Close()
+		log.Println("response Status:", resp.Status)
+		log.Println("response Headers:", resp.Header)
+		// body, _ := ioutil.ReadAll(resp.Body)
+		// var obj interface{}
+		word, err := NewWord(c, resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		return &word, nil
+		// err = json.NewDecoder(resp.Body).Decode(&obj)
+		// log.Println("response Body:", obj)
+		// return &obj, nil
+	}
 	return &word, nil
 }
