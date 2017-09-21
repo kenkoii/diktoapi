382b90ba787fd0b42331d8f36c35b44ccfaf5c64
Kentoy <jankenith.johannessen@gmail.com>
2017-09-21 12:21:53 +0800

    css fix and inifinite scroll

--- app/main.go
+++ app/main.go
@@ -16,9 +16,10 @@ func GetMainEngine() *gin.Engine {
 	router := gin.Default()
 	// gin.SetMode(gin.ReleaseMode)
 	router.Use(middlewares.CORSMiddleware())
-	router.Static("assets", "../api/assets")
-	router.LoadHTMLGlob("../api/templates/*")
-	// router.LoadHTMLGlob("/go/src/github.com/kenkoii/diktoapi/api/templates/*")
+	router.Static("assets", "assets")
+	router.LoadHTMLGlob("templates/*")
+	// router.LoadHTMLGlob("github.com/kenkoii/diktoapi/api/templates/*")
+	// router.LoadHTMLGlob(filepath.Join(os.Getenv("GOPATH"), "src/github.com/kenkoii/diktoapi/api/templates/*"))
 	router = routers.InitGinRoutes(router)
 	return router
 }
